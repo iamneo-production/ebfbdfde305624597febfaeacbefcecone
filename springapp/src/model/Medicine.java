02507fb36ed6d02e0cf3f84d824f978bea60cda4
Iamneo <techadmin@iamneo.ai>
2023-09-30 13:55:29 +0000

    Backup Commit

---
+++ springapp/src/model/Medicine.java
@@ -0,0 +1,22 @@
+package com.yourpackage.model;
+
+public class Medicine {
+    private int medicineId;
+    private String medicineName;
+    private float price;
+    private int quantity;
+    private String description;
+
+    public Medicine() {
+    }
+
+    public Medicine(int medicineId, String medicineName, float price, int quantity, String description) {
+        this.medicineId = medicineId;
+        this.medicineName = medicineName;
+        this.price = price;
+        this.quantity = quantity;
+        this.description = description;
+    }
+
+    // Getters and setters
+}
